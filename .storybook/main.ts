e5bd687e12eab2c28301418fd931bffb37b72b73
rheasan <rheasan5218@gmail.com>
2025-02-27 13:23:02 +0530

    Initial commit
    Generated by create-expo-app 3.2.0.

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import { StorybookConfig } from "@storybook/react-native-web-vite";
+
+const main: StorybookConfig = {
+  stories: [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+  ],
+
+  framework: {
+    name: "@storybook/react-native-web-vite",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen",
+  },
+};
+
+export default main;
